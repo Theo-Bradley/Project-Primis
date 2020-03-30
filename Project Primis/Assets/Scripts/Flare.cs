23ae2f79080e958ac5a62f7f7bdbdb4f5e703428
Theo-Proulx <gaminggekoman@gmail.com>
2020-03-30 19:45:22 +0100

    Created Functioning Flares
    Added Flares that activate platforms so they can be collided with once they contact eachother

---
+++ Project Primis/Assets/Scripts/Flare.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flare : MonoBehaviour
+{
+
+    Light FlareLight;
+    public float speed;
+    public float offset = 0.3f;
+
+    void Start()
+    {
+        FlareLight = transform.gameObject.AddComponent<Light>();
+        FlareLight.type = LightType.Point;
+        FlareLight.intensity = 1;
+        FlareLight.areaSize = Vector2.one * 5; //Editor Only
+        FlareLight.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z - offset);
+    }
+
+    void Update()
+    {
+        FlareLight.color = Color.red;
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Platform"))
+            col.gameObject.GetComponent<Platform>().isLit(true);
+    }
+}
---
+++ Project Primis/Assets/Scripts/Platform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void isLit(bool isLit)
+    {
+        transform.gameObject.layer = 0; //set collision layer to 0 so it can collide with everything but an object in the "invisible" layer
+    }
+}
