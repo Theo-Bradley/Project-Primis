0b3e79937f2d3f8702726584fd0065a513141bb4
Theo-Proulx <gaminggekoman@gmail.com>
2020-03-31 12:03:17 +0100

    Added spawning for flares
    Click Left Mouse to spawn a flare or change the SpawnKey variable in the inspector to change the spawn key.
    Flares spawn with a random velocity (applies in both directions).

--- Project Primis/Assets/Scripts/ClickToSpawn.cs
+++ Project Primis/Assets/Scripts/ClickToSpawn.cs
@@ -6,14 +6,17 @@ public class ClickToSpawn : MonoBehaviour
 {
 
     public GameObject SpawnObject;
+    public Vector2 TurnMax;
+    public KeyCode SpawnKey;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse1))
+        if (Input.GetKeyDown(SpawnKey))
         {
             var objRef = Instantiate(SpawnObject);
             var tempPos = Camera.main.ScreenToWorldPoint(Input.mousePosition, Camera.MonoOrStereoscopicEye.Mono);
             objRef.transform.position = new Vector3(tempPos.x, tempPos.y, 0);
+            objRef.GetComponent<Rigidbody2D>().AddTorque(Random.Range(TurnMax.x, TurnMax.y));
         }
         
     }
--- Project Primis/Assets/Scripts/FlareLight.cs
+++ Project Primis/Assets/Scripts/FlareLight.cs
@@ -7,12 +7,13 @@ public class FlareLight : MonoBehaviour
 
     Light FlareLightRef;
     public Vector3 offset;
+    public float intensity = 2;
 
     void Start()
     {
         FlareLightRef = transform.gameObject.AddComponent<Light>();
         FlareLightRef.type = LightType.Point;
-        FlareLightRef.intensity = 2;
+        FlareLightRef.intensity = intensity;
         FlareLightRef.areaSize = Vector2.one * 5; //Editor Only
         FlareLightRef.transform.position = new Vector3(transform.position.x, transform.position.y, transform.position.z) + offset;
     }
