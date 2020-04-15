1cca23d9ac34ae8811fcd5c57798a8ae3818624c
GamingTVreal <57823547+GamingTVreal@users.noreply.github.com>
2020-04-15 12:04:32 -0700

    Flare Counter
    The flare goes down whenever the "Spawn Key" variable is active

---
+++ Project Primis/Assets/Scripts/flares/Flare_Counter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Flare_Counter : MonoBehaviour
+{
+    public int flares;
+    public bool isthrowing;
+    private KeyCode SpawnKey;
+    public Text FlareDisplay;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+      SpawnKey = GameObject.Find("GameManager").GetComponent<ClickToSpawn>().SpawnKey;
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlareDisplay.text = flares.ToString();
+        if (Input.GetKeyDown (SpawnKey) && !isthrowing && flares > 0)
+            {
+            isthrowing = true;
+            flares--;
+            isthrowing = false;
+            }
+    }
+}
