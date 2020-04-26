daf31b2f73f910a04780657b796d43aed873be35
Theo-Proulx <gaminggekoman@gmail.com>
2020-04-26 16:09:47 +0100

    Added smooth camera based zoom and follow
    Has variable zoom speed and variable size limits.
    Also has offset parameter to offset from target.

--- Project Primis/Assets/Scripts/misc/camerafollow.cs
+++ Project Primis/Assets/Scripts/misc/camerafollow.cs
@@ -1,20 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class camerafollow : MonoBehaviour
 {
 	//veery simple for now
 	public Transform target;
-
-
+    private float velocity;
+    public float defaultSize;
+    public float maxSize;
+    public float zoomSpeed;
+    
 	public Vector3 offset;
 
-	void LateUpdate()
-	{
-		this.transform.position = new Vector3(target.position.x, target.position.y, offset.z);
-			
-	}
+    void LateUpdate()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, offset.z);
+        var size = Camera.main.orthographicSize;
+        /*
+        size = (target.GetComponent<Rigidbody2D>().velocity.x) / target.GetComponent<Movement>().speed * maxSize;
+        if (size > defaultSize)
+            Camera.main.orthographicSize = size;
+        else
+            Camera.main.orthographicSize = size;
+        Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, defaultSize, maxSize);
+        */
+        var vel = Mathf.Abs(target.GetComponent<Rigidbody2D>().velocity.x) / target.GetComponent<Movement>().speed - 0.5; //get normalized x velocity
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize + ((float)vel * zoomSpeed), defaultSize, maxSize);
+    }
 }
 
 
